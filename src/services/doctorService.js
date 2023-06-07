37c65a88f859f3c2d9dda3558edaa4bd87680694
quang <mnquang123@gmail.com>
2023-06-08 00:45:35 +0700

    Fix small stuffs

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -21,7 +21,7 @@ let getTopDoctorHome = (limit) => {
                         attributes: ["valueEn", "valueVi"],
                     },
                 ],
-                raw: true,
+                raw: false,
                 nest: true,
             });
             resolve({
@@ -57,19 +57,36 @@ let saveDetailInforDoctor = (inputData) => {
             if (
                 !inputData.doctorId ||
                 !inputData.contentHTML ||
-                !inputData.contentMarkdown
+                !inputData.contentMarkdown ||
+                !inputData.action
             ) {
                 resolve({
                     errCode: 1,
                     errMessage: "Missing parameter",
                 });
             } else {
-                await db.Markdown.create({
-                    contentHTML: inputData.contentHTML,
-                    contentMarkdown: inputData.contentMarkdown,
-                    description: inputData.description,
-                    doctorId: inputData.doctorId,
-                });
+                if (inputData.action === "CREATE") {
+                    await db.Markdown.create({
+                        contentHTML: inputData.contentHTML,
+                        contentMarkdown: inputData.contentMarkdown,
+                        description: inputData.description,
+                        doctorId: inputData.doctorId,
+                    });
+                } else if (inputData.action === "EDIT") {
+                    let doctorMarkdown = await db.Markdown.findOne({
+                        where: { doctorId: inputData.doctorId },
+                        raw: false,
+                    });
+                    if (doctorMarkdown) {
+                        doctorMarkdown.contentHTML = inputData.contentHTML;
+                        doctorMarkdown.contentMarkdown =
+                            inputData.contentMarkdown;
+                        doctorMarkdown.description = inputData.description;
+                        doctorMarkdown.doctorId = inputData.doctorId;
+
+                        await doctorMarkdown.save();
+                    }
+                }
                 resolve({
                     errCode: 0,
                     errMessage: "Save doctor information successfully",
@@ -94,7 +111,7 @@ let getDetailDoctorById = (id) => {
                         id,
                     },
                     attributes: {
-                        exclude: ["password", "image"],
+                        exclude: ["password"],
                     },
                     include: [
                         {
@@ -111,9 +128,15 @@ let getDetailDoctorById = (id) => {
                             attributes: ["valueEn", "valueVi"],
                         },
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true,
                 });
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, "base64").toString(
+                        "binary"
+                    );
+                }
+                if (!data) data = {};
                 resolve({
                     errCode: 0,
                     data,
